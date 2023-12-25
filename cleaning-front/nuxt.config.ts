1447e39974b45631482314ec08fb3087d201fd82
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-12-25 23:40:34 +0700

    Seo#1

--- cleaning-front/nuxt.config.ts
+++ cleaning-front/nuxt.config.ts
@@ -1,13 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt', 'nuxt-simple-sitemap', '@nuxtjs/robots'],
+  site: {
+    url: 'https://cleaning-plum.vercel.app/',
+  },
+  sitemap: {
+    xsl: false
+  },
   css: ['~/assets/main.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'yandex-verification', content: 'c56d4eaf719b065c' }
+      ],
     }
   },
 })
